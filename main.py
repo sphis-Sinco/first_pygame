3441e507ceddf00bc2e666ca69327434a349698f
IdealistCat <mattallan.h@gmail.com>
2025-01-19 15:49:53 -0600

    display width and height variables, move around some variables, and arrow key controls

--- main.py
+++ main.py
@@ -2,17 +2,22 @@
 import pygame, file_manager
 import pygame.draw_py
 
+# screen
+display_width = 640
+display_height = 360
+
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-gameDisplay = pygame.display.set_mode((1280,720))
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 
+# player stuff
 playerImg = pygame.image.load(file_manager.playerAsset('idle'), "player-idle")
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     # poll for events
@@ -27,13 +32,13 @@ while running:
     gameDisplay.blit(playerImg, player_pos)
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
         player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
         player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
+    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         player_pos.x += 300 * dt
 
     # flip() the display to put your work on screen
